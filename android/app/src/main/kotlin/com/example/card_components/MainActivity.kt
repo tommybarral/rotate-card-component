623d2a026a92713d19ecd4748d24d60276d51d9c
Tommybarral <isamutom27@hotmail.com>
2022-07-26 13:45:23 +0200

    starting project

---
+++ android/app/src/main/kotlin/com/example/card_components/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.card_components
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home_screen.dart
@@ -0,0 +1,18 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
